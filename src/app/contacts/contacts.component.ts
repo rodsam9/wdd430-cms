ac52e4bf04e0289e778c9f23fde1c524dc8ed6fa
rodsam9 <sr_odriguez9@yahoo.com>
2022-09-19 20:14:21 -0600

    added header and contact list & detail components

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent implements OnInit {
+  contacts = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
