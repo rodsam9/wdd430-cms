b1b1c35281b28da810e45f239ef14e379480ba2f
rodsam9 <sr_odriguez9@yahoo.com>
2022-09-26 19:56:05 -0600

    week 3 (display contact info on side, add basic components (messages & documents))

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';
+
 
 @Component({
   selector: 'app-contact-list',
@@ -7,6 +8,8 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
     new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '08-496-3771', '../../assets/images/jacksonk.jpg', null),
     new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null)
@@ -16,4 +19,7 @@ export class ContactListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 }
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectedFeatureEvent = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelected(selectedEvent: string) {
+    this.selectedFeatureEvent.emit(selectedEvent);
+  }
 }
